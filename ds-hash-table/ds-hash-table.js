6eddc386caecc38c5c734c8416f787590b954c56
hibaAkroush <hiba.akr@gmail.com>
2017-08-26 10:31:12 +0300

    hash done

--- ds-hash-table/ds-hash-table.js
+++ ds-hash-table/ds-hash-table.js
@@ -1,16 +1,53 @@
 var makeHashTable = function() {
   //Do not change the max!
   var max = 4;
-
+//
+// i think maybe collision resolution is when the hash table changes size depending on how many elements it has 
+// if the hash table has less elements it wil change size to 25%
+// it the hash table is almost full it will change size to 75%
     return {
       _storage: [],
       retrieve: function(key) {
-        return this._storage[hashFn(key, max)];
+      var bucket = this._storage[hashFn(key, max)]
+        // for retrieve if the key does not exist 
+        // return null to user 
+        if(!bucket){
+          return null
+        }
+
+        //then  we loop inside the bucket if the key exists we return it  
+        // creat tuple to check
+        for (var i = 0; i < this._storage[i].length; i++) {
+          var tuple = this._storage[i]
+          if (tuple[0]===key) {
+             return tuple [1]
+          }
+        }
       },
 
       insert: function(key, value) {
-        //your code is here
-        this._storage[hashFn(key, max)] = value;
+        // first we should retrieve the bucket
+        var bucket = this._storage[hashFn(key, max)]
+        // then we should check if the bucket does not exist we should creat it
+        if(!bucket){
+          this._storage[hashFn(key, max)] = []
+        }
+
+        //then  we loop inside the bucket if the key exists we update it  
+        // creat tuple to check
+        // if tuple doesnt exist ceat it and push it in bucket 
+        for (var i = 0; i < this._storage[i].length; i++) {
+          var tuple = this._storage[i]
+          if (tuple[0]===key) {
+             tuple [1] =  value
+          }
+          tuple = [this._storage[hashFn(key, max)],value]
+          this._storage.push(tuple)
+        }
+
+        // if the key does not exist we creat the tuple and push it in the bucket 
+
+
     }
   }
 };
