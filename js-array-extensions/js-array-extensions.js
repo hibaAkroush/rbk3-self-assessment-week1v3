116e4d8acb14599847d83c9976ded26389912a9e
hibaAkroush <hiba.akr@gmail.com>
2017-08-26 10:33:45 +0300

    array extention done

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,16 @@
 // your code here
+var Arrayy = function (arr) {
+	instance = {}
+	instance.first = first
+	instance.last = last
+	instance.arr = arr 
+return instance
+}
+
+this.first = function(){
+	return this.arr[0]
+}
+
+this.last = function(){
+	return this.arr[this.arr.length-1]
+}// your code here
